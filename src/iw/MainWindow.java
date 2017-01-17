9370585fd0054716e5ec1469eb4bd09449dac54d
TheRDavid <david.rosenbusch@googlemail.com>
2017-01-17 01:01:16 +0100

    improved UI & performance

--- src/iw/MainWindow.java
+++ src/iw/MainWindow.java
@@ -11,31 +11,34 @@ import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class MainWindow extends JFrame {
+	int handleSize = 16;
 	private JPanel p = new JPanel() {
 		protected void paintComponent(java.awt.Graphics arg0) {
 			super.paintComponent(arg0);
 			arg0.drawImage(warp, 0, 0, null);
-			for (int i = 0; i < 4; i++) {
-				Point2D.Double po = points[i];
+			for (int i = 0; i < points.length; i += 2) {
 				arg0.setColor(Color.blue);
 				if (i == n)
 					arg0.setColor(Color.RED);
-				arg0.fillRect((int) po.getX() - 5, (int) po.getY() - 5, 10, 10);
+				arg0.fillRect((int) points[i] - handleSize / 2, (int) points[i + 1] - handleSize / 2, handleSize,
+						handleSize);
 			}
 		};
 	};
 	private BufferedImage warp, original;
-	private Point2D.Double points[];
+	private float points[] = new float[8];
 	private int n = 0;
 
 	public MainWindow(BufferedImage i) {
 		warp = i;
 		original = new BufferedImage(i.getWidth(), i.getHeight(), i.getType());
-		points = new Point2D.Double[] { new Point2D.Double(0, 0), new Point2D.Double(original.getWidth(), 0),
-				new Point2D.Double(original.getWidth(), original.getHeight()),
-				new Point2D.Double(0, original.getHeight()) };
+		points[2] = i.getWidth();
+		points[4] = i.getWidth();
+		points[5] = i.getHeight();
+		points[7] = i.getHeight();
 		Graphics g = original.getGraphics();
 		g.drawImage(i, 0, 0, null);
 		g.dispose();
@@ -44,25 +47,25 @@ public class MainWindow extends JFrame {
 			@Override
 			public void mouseDragged(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				points[n] = new Point2D.Double(arg0.getX(), arg0.getY());
-				warp = Start.warp(original, warp, points[0], points[1], points[2], points[3]);
+				points[n] = arg0.getX();
+				points[n + 1] = arg0.getY();
+				Start.warp(original, warp, points[0], points[1], points[2], points[3], points[4], points[5],
+						points[6], points[7]);
 				p.repaint();
 				super.mouseDragged(arg0);
 			}
 		});
 		p.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseReleased(MouseEvent arg0) {
+			public void mousePressed(MouseEvent arg0) {
 				if (arg0.getButton() == MouseEvent.BUTTON1) {
-					for (int i = 0; i < points.length; i++)
-						if (arg0.getX() >= points[i].x - 5 && arg0.getX() <= points[i].x + 5
-								&& arg0.getY() >= points[i].y - 5 && arg0.getY() <= points[i].y + 5)
+					for (int i = 0; i < points.length; i += 2)
+						if (arg0.getX() >= points[i] - handleSize / 2 && arg0.getX() <= points[i] + handleSize / 2
+								&& arg0.getY() >= points[i + 1] - handleSize / 2
+								&& arg0.getY() <= points[i + 1] + handleSize / 2)
 							n = i;
 				} else if (arg0.getButton() == MouseEvent.BUTTON3) {
-					points = new Point2D.Double[] { new Point2D.Double(0, 0),
-							new Point2D.Double(original.getWidth(), 0),
-							new Point2D.Double(original.getWidth(), original.getHeight()),
-							new Point2D.Double(0, original.getHeight()) };
+					points = new float[8];
 					Graphics g = warp.getGraphics();
 					g.drawImage(original, 0, 0, null);
 					g.dispose();
@@ -74,6 +77,7 @@ public class MainWindow extends JFrame {
 		});
 		setSize(original.getWidth() + 100, original.getHeight() + 100);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		p.setBorder(new EmptyBorder(50, 50, 50, 50));
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
--- src/iw/Start.java
+++ src/iw/Start.java
@@ -1,53 +1,56 @@
 package iw;
 
-import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class Start {
-
-	public static BufferedImage warp(BufferedImage original, BufferedImage warp, Point2D.Double A, Point2D.Double B, Point2D.Double C, Point2D.Double D) {
+	private static int[] i = new int[3];
+	private static WritableRaster inRaster, outRaster;
+	public static void warp(BufferedImage original, BufferedImage warp, float ax, float ay, 
+			float bx, float by,float cx, float cy,float dx, float dy) {
 		Graphics g = warp.getGraphics();
 		g.clearRect(0, 0, warp.getWidth(), warp.getHeight()); // clear
+		
+		inRaster  = original.getRaster();
+		outRaster = warp.getRaster();
+	  
 		for (int x = 0; x < original.getWidth(); x++) {
 
-			double x_share = (double) (x) / original.getWidth(); // how far on x axis (of original 0-1)
-			double x_share_comp = (1 - x_share); // diff of 1 and original_x_share
+			float x_share = (float) (x) / original.getWidth(); // how far on x axis (of original 0-1)
+			float x_share_comp = (1 - x_share); // diff of 1 and original_x_share
 
-			double y_start = (A.y * x_share_comp + B.y * x_share); // most left y coordiante
-			double y_end = (D.y * x_share_comp + C.y * x_share); // most right y coordinate
+			float y_start = (ay * x_share_comp + by * x_share); // most left y coordiante
+			float y_end = (dy * x_share_comp + cy * x_share); // most right y coordinate
 
 			for (int y = 0; y < original.getHeight(); y++) {
-				double y_share = (double) (y) / original.getHeight(); // how far on y axis (of original, 0-1)
-				double y_share_comp = (1 - y_share); // diff of 1 and original_y_share
+				float y_share = (float) (y) / original.getHeight(); // how far on y axis (of original, 0-1)
+				float y_share_comp = (1 - y_share); // diff of 1 and original_y_share
 
-				double x_start = (A.x * y_share_comp + D.x * y_share); // most top x coordinate
-				double x_end = (B.x * y_share_comp + C.x * y_share); // most bottom x coordiante
+				float x_start = (ax * y_share_comp + dx * y_share); // most top x coordinate
+				float x_end = (bx * y_share_comp + cx * y_share); // most bottom x coordiante
 
-				double x_length = x_end - x_start;
-				double y_length = y_end - y_start;
+				float x_length = x_end - x_start;
+				float y_length = y_end - y_start;
 
-				double x_new = x_start + x_share * x_length; // position on warped x axis
-				double y_new = y_start + y_share * y_length; // position on warped y axis
+				float x_new = x_start + x_share * x_length; // position on warped x axis
+				float y_new = y_start + y_share * y_length; // position on warped y axis
 				try {
-					warp.setRGB((int) (x_new), (int) (y_new), original.getRGB(x, y)); // waaaaaarp
+					outRaster.setPixel((int)x_new, (int) y_new, inRaster.getPixel(x, y, i));
 				} catch (Exception e) {
 					// whatevs
 				}
 			}
 		}
-		return warp;
 	}
 
 	public static void main(String[] args) {
 		try {
 			BufferedImage original = ImageIO.read(new File("image.png"));
-
 			new MainWindow(original);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
