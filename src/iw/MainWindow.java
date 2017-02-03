3a27c57c68b59006835af35a0986df7b9a7e74a9
TheRDavid <david.rosenbusch@googlemail.com>
2017-02-03 03:41:02 +0100

    refractor, new structure, remove backward mapping

--- src/iw/MainWindow.java
+++ src/iw/MainWindow.java
@@ -10,43 +10,42 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class MainWindow extends JFrame {
 	int handleSize = 16;
-	private BufferedImage warpForward, warpForwardDebug, warpBackward, warpBackwardDebug, original;
-	private BuffViewer warpForwardView, warpBackwardView, warpForwardDebugView, warpBackwardDebugView;
-	private float points[][] = new float[4][2];
+	private BufferedImage transformImage, debugImage, original;
+	private BuffViewer transformPanel, debugPanel;
 	private int n = 0;
+
+	private float distortPoints[][] = new float[4][2];
+
+	private enum modeType {
+		DISTORT, EUCLIDEAN
+	};
+
+	private JComboBox<modeType> modeCombobox = new JComboBox<>(new modeType[] { modeType.DISTORT, modeType.EUCLIDEAN });
 
 	public MainWindow(BufferedImage i) {
 		original = i;
-		warpBackward = new BufferedImage(i.getWidth(), i.getHeight(), i.getType());
-		warpForward = new BufferedImage(i.getWidth(), i.getHeight(), i.getType());
-		warpBackwardDebug = new BufferedImage(i.getWidth(), i.getHeight(), i.getType());
-		warpForwardDebug = new BufferedImage(i.getWidth(), i.getHeight(), i.getType());
-		warpForwardView = new BuffViewer(warpForward);
-		warpForwardDebugView = new BuffViewer(warpForwardDebug);
-		warpBackwardView = new BuffViewer(warpBackward);
-		warpBackwardDebugView = new BuffViewer(warpBackwardDebug);
-		points[1][0] = i.getWidth();
-		points[2][0] = i.getWidth();
-		points[2][1] = i.getHeight();
-		points[3][1] = i.getHeight();
-		Graphics g = warpForward.getGraphics();
+		transformImage = new BufferedImage(i.getWidth(), i.getHeight(), i.getType());
+		debugImage = new BufferedImage(i.getWidth(), i.getHeight(), i.getType());
+		transformPanel = new BuffViewer(transformImage);
+		debugPanel = new BuffViewer(debugImage);
+		distortPoints[1][0] = i.getWidth();
+		distortPoints[2][0] = i.getWidth();
+		distortPoints[2][1] = i.getHeight();
+		distortPoints[3][1] = i.getHeight();
+		Graphics g = transformImage.getGraphics();
 		g.drawImage(i, 0, 0, null);
 		g.dispose();
-		g = warpBackward.getGraphics();
-		g.drawImage(i, 0, 0, null);
-		g.dispose();
-		setLayout(new GridLayout(2, 0,10,10));
 		getContentPane().setBackground(Color.DARK_GRAY);
-		add(warpForwardView);
-		add(warpBackwardView);
-		add(warpForwardDebugView);
-		add(warpBackwardDebugView);
+		add(transformPanel, BorderLayout.NORTH);
+		add(debugPanel, BorderLayout.CENTER);
+		add(modeCombobox, BorderLayout.SOUTH);
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
@@ -55,10 +54,8 @@ public class MainWindow extends JFrame {
 	}
 
 	public void repaintEmAll() {
-		warpForwardView.repaint();
-		warpForwardDebugView.repaint();
-		warpBackwardView.repaint();
-		warpBackwardDebugView.repaint();
+		transformPanel.repaint();
+		debugPanel.repaint();
 	}
 
 	private class BuffViewer extends JPanel {
@@ -70,13 +67,12 @@ public class MainWindow extends JFrame {
 			addMouseMotionListener(new MouseMotionAdapter() {
 				@Override
 				public void mouseDragged(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					points[n][0] = arg0.getX();
-					points[n][1] = arg0.getY();
-					Transformer.warp_forward(original, warpForward, points);
-					Transformer.warp_forward_debug(original, warpForwardDebug, points,100,100);
-					Transformer.warp_backward(original, warpBackward, points);
-					Transformer.warp_backward_debug(original, warpBackwardDebug, points,100,100);
+					if (modeCombobox.getSelectedItem().equals(modeType.DISTORT)) {
+						distortPoints[n][0] = arg0.getX();
+						distortPoints[n][1] = arg0.getY();
+						Transformer.distort(original, transformImage, distortPoints);
+						Transformer.distort_debug(original, debugImage, distortPoints, 70, 70);
+					}
 					repaintEmAll();
 					super.mouseDragged(arg0);
 				}
@@ -84,23 +80,25 @@ public class MainWindow extends JFrame {
 			addMouseListener(new MouseAdapter() {
 				@Override
 				public void mousePressed(MouseEvent arg0) {
-					if (arg0.getButton() == MouseEvent.BUTTON1) {
-						for (int i = 0; i < points.length; i++)
-							if (arg0.getX() >= points[i][0] - handleSize / 2 && arg0.getX() <= points[i][0] + handleSize / 2
-									&& arg0.getY() >= points[i][1] - handleSize / 2
-									&& arg0.getY() <= points[i][1] + handleSize / 2)
-								n = i;
-					} else if (arg0.getButton() == MouseEvent.BUTTON3) {
-						points = new float[4][2];
-						points[1][0] = i.getWidth();
-						points[2][0] = i.getWidth();
-						points[2][1] = i.getHeight();
-						points[3][1] = i.getHeight();
-						Graphics g = warpForward.getGraphics();
-						g.drawImage(original, 0, 0, null);
-						g.dispose();
+					if (modeCombobox.getSelectedItem().equals(modeType.DISTORT)) {
+						if (arg0.getButton() == MouseEvent.BUTTON1) {
+							for (int i = 0; i < distortPoints.length; i++)
+								if (arg0.getX() >= distortPoints[i][0] - handleSize / 2
+										&& arg0.getX() <= distortPoints[i][0] + handleSize / 2
+										&& arg0.getY() >= distortPoints[i][1] - handleSize / 2
+										&& arg0.getY() <= distortPoints[i][1] + handleSize / 2)
+									n = i;
+						} else if (arg0.getButton() == MouseEvent.BUTTON3) {
+							distortPoints = new float[4][2];
+							distortPoints[1][0] = i.getWidth();
+							distortPoints[2][0] = i.getWidth();
+							distortPoints[2][1] = i.getHeight();
+							distortPoints[3][1] = i.getHeight();
+							Graphics g = transformImage.getGraphics();
+							g.drawImage(original, 0, 0, null);
+							g.dispose();
+						}
 					}
-					// TODO Auto-generated method stub
 					repaintEmAll();
 					super.mouseReleased(arg0);
 				}
@@ -110,12 +108,14 @@ public class MainWindow extends JFrame {
 		protected void paintComponent(java.awt.Graphics arg0) {
 			super.paintComponent(arg0);
 			arg0.drawImage(image, 0, 0, null);
-			for (int i = 0; i < points.length; i ++) {
-				arg0.setColor(Color.blue);
-				if (i == n)
-					arg0.setColor(Color.YELLOW);
-				arg0.fillOval((int) points[i][0] - handleSize / 2, (int) points[i][1] - handleSize / 2, handleSize,
-						handleSize);
+			if (modeCombobox.getSelectedItem().equals(modeType.DISTORT)) {
+				for (int i = 0; i < distortPoints.length; i++) {
+					arg0.setColor(Color.blue);
+					if (i == n)
+						arg0.setColor(Color.YELLOW);
+					arg0.fillOval((int) distortPoints[i][0] - handleSize / 2,
+							(int) distortPoints[i][1] - handleSize / 2, handleSize, handleSize);
+				}
 			}
 		};
 
